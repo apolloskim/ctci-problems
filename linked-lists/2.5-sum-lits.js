b60f028d066fdc830fb8d0f20bdc82ba33d33dee
Apollos <apolloskim94@gmail.com>
2019-03-12 12:42:25 -0400

    refactor 2.5: sum-lists to return a linked list node

--- linked-lists/2.5-sum-lits.js
+++ linked-lists/2.5-sum-lits.js
@@ -6,15 +6,20 @@ class listNode {
 } 
 
 function sumLists(node1, node2, extra) {
-
     if (!node1 && !node2) {
         return 0;
     } else if (node1 && !node2) {
-        return node1.value + 10 * sumLists(node1.next, node2, 0);
+        let newNode = new listNode(node1.value);
+        newNode.next = sumLists(node1.next, node2, 0);
+        return newNode;
     } else if (!node1 && node2) {
-        return node2.value + 10 * sumLists(node1, node2.next, 0);
+        let newNode = new listNode(node2.value);
+        newNode.next = sumLists(node1, node2.next, 0);
+        return newNode;
     } else {
-        let sum = node1.value + node2.value;
-        return (sum % 10) + 10 * sumLists(node1.next, node2.next, Math.floor(sum / 10));
+        let sum = node1.value + node2.value + extra;
+        let newNode = new listNode(sum % 10);
+        newNode.next = sumLists(node1.next, node2.next, Math.floor(sum / 10));
+        return newNode;
     }
 }
